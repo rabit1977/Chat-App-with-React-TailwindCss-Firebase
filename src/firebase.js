9ea3318b7332ff4d68ef9e9bde285321e5479884
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-09-18 19:06:23 +0200

    changed some stuff

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
-import firebaseConfig from './firebseConfig';
+import firebaseConfig from '../firebaseConfig';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);

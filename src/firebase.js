39be531a6fa9f9cd92bdcdb58c74ab6e8979fbb1
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-09-21 21:18:28 +0200

    ...

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
-import { firebaseConfig } from './firebaseConfig';
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'react-chat-app-69113.firebaseapp.com',
+  projectId: 'react-chat-app-69113',
+  storageBucket: 'react-chat-app-69113.appspot.com',
+  messagingSenderId: '344537587450',
+  appId: '1:344537587450:web:bfdc67171bfd222d887343',
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
